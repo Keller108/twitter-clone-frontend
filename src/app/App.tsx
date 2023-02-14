f1dd0df46be5efc1b0a9b167890e9c59850ba4f1
Nick <keller108@yandex.ru>
2023-02-14 11:37:56 +0500

    Добавлен компоненет NotFound

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+import { WelcomePage } from "../pages/Welcome";
+import { NotFound } from "../pages/NotFound";
+import { WELCOME_ROUTE } from "../shared/routes";
 import './App.css';
 
 export function App() {
     return (
         <div className="app">
             <Routes>
-                <Route path={} element={} />
+                <Route path={WELCOME_ROUTE} element={<WelcomePage />} />
+                <Route path='*' element={<NotFound />} />
             </Routes>
         </div>
     )
