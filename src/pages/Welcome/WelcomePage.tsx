0b29a07774f5e42d020c8d129aa87dda66642bf8
Nick <keller108@yandex.ru>
2023-02-14 21:49:40 +0500

    Верстка страницы HomePage

--- src/pages/Welcome/WelcomePage.tsx
+++ src/pages/Welcome/WelcomePage.tsx
@@ -1,7 +1,20 @@
+import TWITTER_LOGO_PATH from '../../shared/icons/twitter/twitter-logo.svg';
 import './WelcomePage.css';
 
 export const WelcomePage = () => {
     return (
-        <div className="welcome-page">WelcomePage</div>
+        <div className="welcome-page">
+            <div className="welcome-page__preview"></div>
+            <section className="welcome-page__join">
+                <img
+                    className="welcome-page__twitter-logo"
+                    src={TWITTER_LOGO_PATH}
+                    alt="twitter logo"
+                />
+                <h1 className="welcome-page__heading">Happening now</h1>
+                <p className="welcome-page__description">Присоединиться</p>
+
+            </section>
+        </div>
     )
 }
