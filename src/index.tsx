108c1406b793748a0646fd8f4eef81c73e598e6a
Nick <keller108@yandex.ru>
2023-02-14 11:09:58 +0500

    Удалена лишняя внутренняя директория App

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './app/App';
+import { App } from './app';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 import { compose } from 'redux';
