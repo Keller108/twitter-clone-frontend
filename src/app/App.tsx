71aee49dc7eb462ccd977cdf40db1363133113df
Nick <keller108@yandex.ru>
2023-02-14 11:28:22 +0500

    Добавлены изначальные стили

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,9 @@
-import { Fragment, useEffect, useState } from 'react';
-import { Preloader } from '../shared/ui/Preloader';
 import './App.css';
 
 export function App() {
-    const [isLoading, setIsLoading] = useState(false);
-    useEffect(() => {
-        setIsLoading(true);
-        setTimeout(() => {
-            setIsLoading(false);
-        }, 400);
-    }, [])
-    return (<Fragment>
-            {isLoading
-                ? <Preloader />
-                : <div>Hello World!</div>
-            }
-        </Fragment>
+    return (
+        <div className="app">
+            Hello World!
+        </div>
     )
 }
