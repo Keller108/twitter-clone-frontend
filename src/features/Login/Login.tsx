fe5a074b58f5e80b3c5ea4eb19f31592b1a8cbe5
Nick <keller108@yandex.ru>
2023-02-18 19:42:23 +0500

    Начало реализации компонента страницы HomePage

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,12 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+import { HOME_ROUTE } from '../../shared/routes';
 import { Form } from '../../shared/ui/Form';
 import './Login.css';
 
 export const Login = () => {
+    const navigate = useNavigate();
     return (
         <Form
             formTitle="Авторизоваться"
             btnText="Авторизация"
-            onSubmit={() => 1}
+            onSubmit={() => navigate(HOME_ROUTE)}
         />
     )
 }
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -2,6 +2,16 @@ import './HomePage.css';
 
 export const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <div className="home-page">
+            <aside className="features">
+
+            </aside>
+            <main className="twittes">
+
+            </main>
+            <aside className="news">
+
+            </aside>
+        </div>
     )
 }
