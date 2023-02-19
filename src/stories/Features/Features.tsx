2ede6684484feaf01a34a31bcf17d840361c0dc7
Nick <keller108@yandex.ru>
2023-02-19 15:06:14 +0500

    Реализации стори Features

---
+++ src/stories/Features/Features.tsx
@@ -0,0 +1,11 @@
+import './Features.css';
+
+export const Features = () => {
+    return (
+        <ul className="feature-item">
+            <li className="feature-item">
+
+            </li>
+        </ul>
+    )
+}
