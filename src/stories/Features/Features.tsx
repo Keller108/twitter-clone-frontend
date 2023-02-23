8db3d4d9186b171dbc605dd07175ff8a2478fda6
Nick <keller108@yandex.ru>
2023-02-23 11:12:20 +0500

    Подправлены цвета иконо

--- src/stories/Features/Features.tsx
+++ src/stories/Features/Features.tsx
@@ -7,13 +7,11 @@ export const Features = () => {
             title: 'Home',
             value: 'home',
             link: '/home',
-
         },
         profile: {
             title: 'Profile',
             value: 'profile',
             link: '/profile',
-
         }
     };
 
