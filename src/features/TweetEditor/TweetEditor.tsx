8651efadc933b7644ff332dd5f955a1c733b09be
Nick <keller108@yandex.ru>
2023-02-26 10:55:46 +0300

    Сверстан компонент TweeterEditor

--- src/features/TweetEditor/TweetEditor.tsx
+++ src/features/TweetEditor/TweetEditor.tsx
@@ -1,7 +1,28 @@
 import './TweetEditor.css';
 import PERSON_PATH from './assets/profile_img.png';
+import MEDIA_PATH from './assets/icon_image.svg';
+import { Button } from '../../shared/ui/Button';
+
+interface ITweet {
+    action: () => void;
+    icon: string;
+}
+
+interface ITweeterActionItems {
+    [key: string]: ITweet
+}
 
 export const TweetEditor = () => {
+    const tweeterActionsMap: ITweeterActionItems = {
+        media: {
+            action: () => 1,
+            icon: MEDIA_PATH
+        }
+    };
+
+    const tweeterActions = Object.entries(tweeterActionsMap)
+        .map(([key, value]) => ({ key, value}));
+
     return (
         <div className="tweet-editor">
             <img className="tweet-editor__avatar"
@@ -14,11 +35,22 @@ export const TweetEditor = () => {
                     rows={4}
                     placeholder="What's happening?"
                 />
-                <ul className="tweet-editor__actions">
-                    <li className="tweet-editor__action">
-
-                    </li>
-                </ul>
+                <div className="tweet-editor__features">
+                    <ul className="tweet-editor__actions">
+                        {tweeterActions.map(({ value }) => <li
+                            className="tweet-editor__action">
+                                <img src={value.icon} alt="tweeterAction" />
+                            </li>)
+                        }
+                    </ul>
+                    <button
+                        className="tweet-editor__action-btn"
+                        type="button"
+                        onClick={() => 1}
+                    >
+                        Tweet
+                    </button>
+                </div>
             </div>
         </div>
     )
