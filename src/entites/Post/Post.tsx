fbcd8649ca18418070f99c532a09733fb61ef596
Nick <keller108@yandex.ru>
2023-02-26 18:26:25 +0300

    Реализация модели Post

--- src/entites/Post/Post.tsx
+++ src/entites/Post/Post.tsx
@@ -1,7 +1,22 @@
 import './Post.css';
+import AVATAR_PATH from './assets/profile_img.png';
+import PIC_PATH from './assets/pic-twit-1.jpg';
 
 export const Post = () => {
     return (
-        <div>Post</div>
+        <li className="post">
+            <img className="post__avatar" src={AVATAR_PATH} alt="avatar" />
+            <div className="post__content">
+                <div className="post__header">
+                    <div className="post__details">
+                        <h2 className="post__user-name">Devon Lane</h2>
+                        <p className="post__account-name">@devondude</p>
+                        <p className="post__created-date">~1 hour ago</p>
+                    </div>
+                    <p className="post__message">Tom is in a big hurry.</p>
+                </div>
+                <img className="post__picture" src={PIC_PATH} alt="post" />
+            </div>
+        </li>
     )
 }
---
+++ src/features/PostList/PostList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const PostList = () => {
+  return (
+    <ul className="post-list">
+
+    </ul>
+  )
+}
