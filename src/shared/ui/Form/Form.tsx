ac5224173943ae4f8780c93d2195833433a26397
Nick <keller108@yandex.ru>
2023-03-01 17:01:54 +0300

    Написаны хендлеры для формы для создания юзера

--- src/shared/ui/Form/Form.tsx
+++ src/shared/ui/Form/Form.tsx
@@ -1,4 +1,6 @@
-import { Fragment } from 'react';
+import { ChangeEvent, Fragment, useState } from 'react';
+import { createUser } from '../../../services/actions/user';
+import { useDispatch, useSelector } from '../../hooks';
 import { Button } from '../Button';
 import './Form.css';
 
@@ -10,19 +12,72 @@ type Props = {
 };
 
 export const Form = ({ formTitle, btnText, onSubmit, isRegister = false }: Props) => {
+    const [user, setUser] = useState({
+        userName: '',
+        email: '',
+        password: '',
+        avatar: ''
+    });
+
+    const dispatch = useDispatch();
+
+    const setName = (evt: ChangeEvent<HTMLInputElement>) => setUser((prevState) => ({
+        ...prevState,
+        userName: evt.target.value
+    }));
+
+    const setEmail = (evt: ChangeEvent<HTMLInputElement>) => setUser((prevState) => ({
+        ...prevState,
+        email: evt.target.value
+    }));
+
+    const setPassword = (evt: ChangeEvent<HTMLInputElement>) => setUser((prevState) => ({
+        ...prevState,
+        password: evt.target.value
+    }));
+
+    const setAvatar = (evt: ChangeEvent<HTMLInputElement>) => setUser((prevState) => ({
+        ...prevState,
+        avatar: evt.target.value
+    }));
+
+    const onFormSubmit = () => {
+        dispatch(createUser(user));
+    };
+
     return (
         <form onSubmit={onSubmit} className="form">
             <h2 className="form__title">{formTitle}</h2>
             <label className="form__label" htmlFor="name">Ваше имя</label>
-            <input className="form__input" id="name" type="text" />
+            <input
+                onChange={setName}
+                className="form__input"
+                id="name"
+                type="text"
+            />
             <label className="form__label" htmlFor="name">Ваша почта</label>
-            <input className="form__input" id="name" type="email" />
+            <input
+                onChange={setEmail}
+                className="form__input"
+                id="name"
+                type="email"
+            />
             <label className="form__label" htmlFor="name">Ваш пароль</label>
-            <input className="form__input" id="name" type="password" />
+            <input
+                onChange={setPassword}
+                className="form__input"
+                id="name"
+                type="password"
+            />
             {isRegister &&
                 <Fragment>
                     <label className="form__label" htmlFor="avatar">Ссылка на аватар</label>
-                    <input className="form__input" id="avatar" type="text" />
+                    <input
+                        onChange={setAvatar}
+                        className="form__input"
+                        id="avatar"
+                        type="text"
+                    />
                 </Fragment>
             }
             <Button
