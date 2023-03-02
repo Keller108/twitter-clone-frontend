b0ab5438163cd8cbc16d32edc5e96d98ad31ed5b
Nick <keller108@yandex.ru>
2023-03-02 10:22:09 +0300

    Разработка TweetModal

--- src/features/TweetEditor/TweetEditor.tsx
+++ src/features/TweetEditor/TweetEditor.tsx
@@ -1,8 +1,8 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from '../../shared/hooks';
 import MEDIA_PATH from './assets/icon_image.svg';
 import { ITweetActionItems } from './types';
 import { TweetActions } from './ui/TweetActions';
-import { useState } from 'react';
-import { useDispatch, useSelector } from '../../shared/hooks';
 import { postMessage } from '../../services/actions/post';
 import { v4 as uuidv4 } from 'uuid';
 import './TweetEditor.css';
@@ -10,10 +10,9 @@ import { FormInput } from '../../shared/ui/FormInput';
 
 export const TweetEditor = () => {
     const [message, setMessage] = useState("");
-    const [imgModalActive, setImgModalActive] = useState(false);
+    const [tweetModalActive, setTweetModalActive] = useState(false);
     const [, setMediaLink] = useState("");
 
-    const { isModalActive } = useSelector(store => store.modalStore);
     const { userName, avatar } = useSelector(store => store.userStore.user);
 
     const dispatch = useDispatch();
---
+++ src/features/TweetEditor/ui/TweetModal/types/index.ts
@@ -0,0 +1,1 @@
+export { TweetModalTypes } from './TweetModalTypes';
